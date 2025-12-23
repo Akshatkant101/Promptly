3b10a35fc41ea52156b32959d63c3c2d7d0b9ead
Akshatkant101 <akshatkant12@gmail.com>
2025-12-23 18:53:26 +0530

    Initialised db

--- client/app/page.tsx
+++ client/app/page.tsx
@@ -1,9 +1,15 @@
+"use client"
 import { Button } from "@/components/ui/button";
 
 export default function Home() {
+
+  const handlesubmit = () => {
+    console.log("Button clicked");
+  }
+
   return (
     <div className="flex min-h-screen items-center justify-center">
-      <Button>
+      <Button onClick={handlesubmit}>
         Click me
       </Button>
     </div>
